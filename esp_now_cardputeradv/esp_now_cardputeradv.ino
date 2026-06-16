f56a8058f3842dc4cabbca3a765ea3039373bd73
GraysonReid <gb.games-main@outlook.com>
2026-06-15 17:08:59 -0700

    Add files via upload

---
+++ esp_now_cardputeradv/esp_now_cardputeradv.ino
@@ -0,0 +1,167 @@
+#include <M5Cardputer.h>
+#include <WiFi.h>
+#include <esp_now.h>
+
+String inputLine = "";
+String history[20];
+int historyCount = 0;
+
+uint8_t broadcastAddress[] = {
+  0xFF, 0xFF, 0xFF,
+  0xFF, 0xFF, 0xFF
+};
+
+String username = "Cardputer";
+
+void addMessage(String msg)
+{
+  if (historyCount < 20)
+  {
+    history[historyCount++] = msg;
+  }
+  else
+  {
+    for (int i = 0; i < 19; i++)
+    {
+      history[i] = history[i + 1];
+    }
+
+    history[19] = msg;
+  }
+}
+
+void redraw()
+{
+  M5Cardputer.Display.fillScreen(BLACK);
+
+  int y = 2;
+
+  for (int i = 0; i < historyCount; i++)
+  {
+    M5Cardputer.Display.drawString(history[i], 2, y);
+    y += 10;
+  }
+
+  M5Cardputer.Display.fillRect(
+    0,
+    M5Cardputer.Display.height() - 14,
+    M5Cardputer.Display.width(),
+    14,
+    DARKGREY
+  );
+
+  M5Cardputer.Display.drawString(
+    ">" + inputLine,
+    2,
+    M5Cardputer.Display.height() - 12
+  );
+}
+
+void onDataRecv(
+  const esp_now_recv_info_t *info,
+  const uint8_t *incomingData,
+  int len)
+{
+  String msg = "";
+
+  for (int i = 0; i < len; i++)
+  {
+    msg += (char)incomingData[i];
+  }
+
+  addMessage(msg);
+  redraw();
+}
+
+void onDataSent(
+  const wifi_tx_info_t *info,
+  esp_now_send_status_t status)
+{
+  // Optional send status handling
+}
+
+void setup()
+{
+  auto cfg = M5.config();
+
+  M5Cardputer.begin(cfg, true);
+
+  M5Cardputer.Display.setRotation(1);
+  M5Cardputer.Display.setTextSize(1);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK)
+  {
+    M5Cardputer.Display.println("ESP-NOW FAIL");
+
+    while (true)
+    {
+      delay(1000);
+    }
+  }
+
+  esp_now_register_recv_cb(onDataRecv);
+  esp_now_register_send_cb(onDataSent);
+
+  esp_now_peer_info_t peerInfo = {};
+
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  esp_now_add_peer(&peerInfo);
+
+  addMessage("ESP-NOW CHAT READY");
+  addMessage("MAC:");
+  addMessage(WiFi.macAddress());
+
+  redraw();
+}
+
+void loop()
+{
+  M5Cardputer.update();
+
+  if (M5Cardputer.Keyboard.isChange())
+  {
+    Keyboard_Class::KeysState status =
+      M5Cardputer.Keyboard.keysState();
+
+    for (auto c : status.word)
+    {
+      inputLine += c;
+    }
+
+    if (status.del)
+    {
+      if (inputLine.length() > 0)
+      {
+        inputLine.remove(inputLine.length() - 1);
+      }
+    }
+
+    if (status.enter)
+    {
+      if (inputLine.length() > 0)
+      {
+        String packet =
+          username +
+          ": " +
+          inputLine;
+
+        addMessage(packet);
+
+        esp_now_send(
+          broadcastAddress,
+          (uint8_t*)packet.c_str(),
+          packet.length()
+        );
+
+        inputLine = "";
+      }
+    }
+
+    redraw();
+  }
+}
